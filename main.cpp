8eacd8c59d3cfdaa1b7450b8f04de1e594d59846
Andrii Honcharov (Luxoft) <AHoncharov@luxoft.com>
2019-08-30 17:49:17 +0300

    Experiments with interface of class

--- main.cpp
+++ main.cpp
@@ -8,20 +8,34 @@
 #include <regex>
 #include <string>
 
-struct Data
+template<typename ID,
+         typename ...Tuples>
+class   TestClass
 {
-    bool mIsRed;
+private:
+
+    std::tuple<Tuples...> tpl;
+    std::vector<ID> vec;
+
+public:
+    template <size_t N>
+    TestClass(ID (&arr)[N], Tuples&&... mem) :
+        tpl{std::forward<Tuples>(mem)...}
+    {
+        static_assert(N == std::tuple_size<std::tuple<Tuples...>>::value, "Size Array of ID and tuples must be equal");
+
+        for(auto & i : arr)
+        {
+            vec.push_back(i);
+        }
+    }
+
+    template <typename... Args>
+    std::vector<ID> calcCoverage(Args&& ... args)
+    {
+
+    }
 };
-
-bool checkIsRed(const Data & data)
-{
-    return data.mIsRed;
-}
-
-bool isSmaller(const size_t & w1, const size_t & w2)
-{
-    return w1 < w2;
-}
 
 template<typename T>
 void customQuickSort(T *array, size_t sizeOfArray)
@@ -63,53 +77,9 @@ void customQuickSort(T *array, size_t sizeOfArray)
 
 int main()
 {
-    /*size_t N = 3;
-    int *A = new int[N];
+    int v []= {1, 2};
 
-    for (size_t i = 0; i < N; ++i)
-    {
-        A[i] = rand() % 10;
-    }
-
-    customQuickSort<int>(A, N);*/
-
-    CoverageCalculator<int, std::string, int, double> calc("one", 2, 66.0);
-    calc.addObject(2, "two", 2, 65.0);
-    calc.addObject(1, "one", 1, 66.0);
-    calc.calculateCoverage();
-
-    /*std::string begin = "begin";
-    Data d1;
-    d1.mIsRed = false;
-
-    std::string A = "A";
-    Data d2;
-    d2.mIsRed = false;
-
-    std::string B = "B";
-    Data d3;
-    d3.mIsRed = false;
-
-    std::string end = "end";
-    Data d4;
-    d4.mIsRed = true;
-
-    IGraph<std::string, Data, size_t> *graph = new GraphImpl<std::string, Data>();
-    graph->addNode(begin, std::move(d1));
-    graph->addNode(A, std::move(d2));
-    graph->addNode(B, std::move(d3));
-    graph->addNode(end, std::move(d4));
-
-    bool resAddEdge1 = graph->addEdge(begin, A, 6);
-    bool resAddEdge2 = graph->addEdge(begin, B, 2);
-    bool resAddEdge3 = graph->addEdge(B, A, 3);
-    bool resAddEdge4 = graph->addEdge(A, end, 1);
-    bool resAddEdge5 = graph->addEdge(B, end, 5);
-
-
-    std::pair<bool, std::string> res1 =  breadthFirstSearch<std::string, Data>(*graph, begin, checkIsRed);
-
-    std::pair<bool, std::vector<std::string>> res2 = DijkstraAlgorithm<std::string, Data, size_t>(*graph, begin, end, isSmaller);*/
+    TestClass<int, std::tuple<int, double, char>, std::tuple<int, double>> testClass(v, std::tuple<int, double, char>(1, 1.5, 'c'), std::tuple<int, double>(1, 1.5));
 
     return 0;
 }
