c58edd54d1c72f44996ef28411a735f9b84ebecc
Andrii Honcharov (Luxoft) <AHoncharov@luxoft.com>
2019-09-02 18:07:11 +0300

    start implementation

--- AlgorithmExercises.pro
+++ AlgorithmExercises.pro
@@ -2,6 +2,7 @@ TEMPLATE = app
 CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
+CONFIG += c++1z
 
 SOURCES += \
         main.cpp
--- coveragecalculator.h
+++ coveragecalculator.h
@@ -5,30 +5,155 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <algorithm>
+#include <experimental/optional>
+#include <tuple>
 
-template <typename ID, typename Props>
+namespace unique_template_types
+{
+    template <typename... Types>
+    struct UniqueTypes;
+
+    template <typename T1, typename T2, typename ... Types>
+    struct UniqueTypes<T1, T2, Types ...>
+     : UniqueTypes<T1, T2>, UniqueTypes<T1, Types ...>, UniqueTypes<T2, Types ...> {};
+
+    template <typename T, typename U>
+    struct UniqueTypes<T, U>
+    {
+        static_assert(!std::is_same<T, U>::value, "Types must be unique");
+    };
+}
+
+namespace check_is_optional_templates
+{
+
+
+    template<typename TypeListOne,
+             template <typename...> class FirstTuple>
+    struct OptionalTypes
+    {
+
+    };
+
+
+}
+
+namespace two_tuples_for_each
+{
+    template <typename... >
+    struct Typelist {};
+
+    template<int index,
+             typename TypeListOne,
+             typename TypeListTwo,
+             template <typename...> class FirstTuple,
+             template <typename...> class SecondTuple,
+             typename Callback>
+    struct iterate_tuple;
+
+    template<int index,
+             typename... FirstTupleArgs,
+             typename... SecondTupleArgs,
+             template <typename...> class FirstTuple,
+             template <typename...> class SecondTuple,
+             typename Callback>
+    struct iterate_tuple<index,
+            Typelist <FirstTupleArgs...>,
+            Typelist <SecondTupleArgs...>,
+            FirstTuple,
+            SecondTuple,
+            Callback>
+    {
+        static void next(const FirstTuple<FirstTupleArgs...>& firstTuple, const SecondTuple<SecondTupleArgs...>& secondTuple, std::vector<bool> & matchingValues, Callback callback)
+        {
+            iterate_tuple<index - 1,
+                    Typelist <FirstTupleArgs...>,
+                    Typelist <SecondTupleArgs...>,
+                    FirstTuple,
+                    SecondTuple, Callback>::next(firstTuple, secondTuple, matchingValues, callback);
+
+            callback(index, std::get<index>(firstTuple), secondTuple, matchingValues);
+        }
+    };
+
+    template<typename... FirstTupleArgs,
+             typename... SecondTupleArgs,
+             template <typename...> class FirstTuple,
+             template <typename...> class SecondTuple,
+             typename Callback>
+    struct iterate_tuple<0,
+            Typelist <FirstTupleArgs...>,
+            Typelist <SecondTupleArgs...>,
+            FirstTuple,
+            SecondTuple,
+            Callback>
+    {
+        static void next(const FirstTuple<FirstTupleArgs...>& firstTuple, const SecondTuple<SecondTupleArgs...>& secondTuple, std::vector<bool> & matchingValues, Callback callback)
+        {
+            callback(0, std::get<0>(firstTuple), secondTuple, matchingValues);
+        }
+    };
+
+    template<typename... FirstTupleArgs,
+             typename... SecondTupleArgs,
+             template <typename...> class FirstTuple,
+             template <typename...> class SecondTuple,
+             typename Callback>
+    struct iterate_tuple<-1,
+            Typelist <FirstTupleArgs...>,
+            Typelist <SecondTupleArgs...>,
+            FirstTuple,
+            SecondTuple,
+            Callback>
+    {
+        static void next(const FirstTuple<FirstTupleArgs...>& firstTuple, const SecondTuple<SecondTupleArgs...>& secondTuple, std::vector<bool> & matchingValues, Callback callback){ }
+    };
+
+    template <typename... FirstTupleArgs, template <typename...> class FirstTuple,
+              typename... SecondTupleArgs, template <typename...> class SecondTuple,
+              typename Callback>
+    void for_each(const FirstTuple<FirstTupleArgs...> & firstTuple, const SecondTuple<SecondTupleArgs...> & secondTuple, std::vector<bool> & matchingValues, Callback callback)
+    {
+        int const t_size = std::tuple_size<std::tuple<FirstTupleArgs...>>::value;
+
+        iterate_tuple<t_size - 1,
+                Typelist <FirstTupleArgs...>,
+                Typelist <SecondTupleArgs...>,
+                FirstTuple,
+                SecondTuple, Callback>::next(firstTuple, secondTuple, matchingValues, callback);
+    }
+};
+
+template <size_t N, typename ID, typename ...Properties>
 class CoverageCalculator
 {
 private:
-    std::unordered_map<ID, std::unordered_set<Props>> mStorage;
-    std::unordered_set<Props> mPropertiesNeeded;
+    std::unordered_map<ID, std::tuple<Properties...>> mStorage;
+    std::unordered_map<ID, std::array<bool, N>> mValidProperties;
+    std::tuple<Properties...> mPropertiesNeeded;
 
 public:
-    template <typename Iterator>
-    CoverageCalculator(const Iterator & begin, Iterator end) :
+    CoverageCalculator(Properties&& ...props) :
         mStorage{},
-        mPropertiesNeeded{begin, end}
+        mPropertiesNeeded{std::forward<Properties...>(props)...}
     {}
 
-    template <typename Iterator>
-    void addObject(const ID & id, const Iterator & begin, const Iterator & end)
+    template <typename ...OptionProperties>
+    void addObject(const ID & id, OptionProperties&& ...optionProperties)
     {
-        mStorage[id] = std::unordered_set<Props>(begin, end);
+        unique_template_types::UniqueTypes<Properties...> checkIsUnique;
+
+        std::tuple<OptionProperties...> optProps(std::forward<OptionProperties...>(optionProperties)...);
+
+
+
+        //mStorage[id] = std::unordered_set<Props>(begin, end);
     }
 
     bool eraseObject(const ID & id)
     {
         size_t tmp = mStorage.erase(id); // std::size_type ??????
+        mValidProperties.erase(id);
 
         return tmp > 0 ? true : false;
     }
@@ -36,13 +161,14 @@ public:
     void clear()
     {
         mStorage.clear();
+        mValidProperties.clear();
     }
 
     std::unordered_set<ID> calculateCoverage()
     {
         std::unordered_set<ID> retVal;
 
-        std::unordered_set<Props> tmpPropertiesNeeded = mPropertiesNeeded;
+        /*std::unordered_set<Props> tmpPropertiesNeeded = mPropertiesNeeded;
 
         for(size_t i = 0; i < mStorage.size() && tmpPropertiesNeeded.size() > 0; ++i)
         {
@@ -73,7 +199,7 @@ public:
                                 std::inserter(diff, diff.begin()));
 
             tmpPropertiesNeeded = diff;
-        }
+        }*/
 
         return retVal;
     }
